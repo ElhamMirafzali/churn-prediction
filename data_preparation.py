342ca5441e80a0c13a6807e4c3f7aad51e98e869
ElhamMirafzali <e.mirafzali@gmail.com>
2020-01-19 22:12:39 +0330

    spliting train and test data

--- data_preparation.py
+++ data_preparation.py
@@ -1,23 +1,20 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
-def reduce_labels_dataset(csv_path):
+def reduce_labels_dataset(csv_path, churn_sample_fraction, non_churn_sample_fraction):
     # csv_path='data/february_labels.csv'
     labels = pd.read_csv(csv_path)
     churn = labels[labels['is_churn'] == 1]
     non_churn = labels[labels['is_churn'] == 0]
-    churn_sample = churn.sample(frac=0.8)
-    non_churn_sample = non_churn.sample(frac=0.05)
+    churn_sample = churn.sample(frac=churn_sample_fraction)
+    non_churn_sample = non_churn.sample(frac=non_churn_sample_fraction)
     labels_reduced = pd.concat([churn_sample, non_churn_sample], ignore_index=True)
     return labels_reduced
 
 
-def find_transactions(labels_path, transactions_path):
-    labels = pd.read_csv(labels_path)
-    transactions = pd.read_csv(transactions_path)
-    i1 = labels.set_index('msno').index
-    i2 = transactions.set_index('msno').index
-    new_transactions = transactions[i2.isin(i1)]
+def find_transactions(transactions, users):
+    new_transactions = common_column(transactions, users, 'msno')
     return new_transactions
 
 
@@ -46,16 +43,19 @@ def transactions_in_range(trans_path, lower_bound, upper_bound):
     # trans_in_range.to_csv('data/transactions_in_march.csv', index=False)
     return trans_in_range
 
+
 #####################################################################
 
 # ------------------------ to reduce the labels dataset
-# targets = reduce_labels_dataset(csv_path='data/february_labels.csv')
+# targets = reduce_labels_dataset(csv_path='data/february_labels.csv',
+#                                  churn_sample_fraction=0.8, non_churn_sample_fraction=0.05)
 # targets.to_csv(path_or_buf='data/february_labels_reduced.csv', index=False)
 
 
 # ------------------------ to find transactions of users in the reduced labels dataset
-# t = find_transactions(labels_path='data/february_labels_reduced.csv',
-#                       transactions_path='data/transactions_in_january.csv')
+# l = pd.read_csv('data/february_labels_reduced.csv')
+# t = pd.read_csv('data/transactions_in_january.csv')
+# t = find_transactions(transactions=t, users=l)
 # t.to_csv(path_or_buf='data/transactions_in_january_reduced.csv', index=False)
 
 
@@ -65,7 +65,23 @@ def transactions_in_range(trans_path, lower_bound, upper_bound):
 #                                               transactions_count_limit=2)
 # t_reduced = pd.read_csv('data/transactions_in_january_reduced.csv')
 # l_reduced = pd.read_csv('data/february_labels_reduced.csv')
-# tr_concat = pd.concat([tr, t_reduced], ignore_index=True, sort=False)
-# lb_concat = pd.concat([lb, l_reduced], ignore_index=True, sort=False)
+# tr_concat = (pd.concat([tr, t_reduced], ignore_index=True, sort=False)).drop_duplicates(keep='first', inplace=False)
+# lb_concat = (pd.concat([lb, l_reduced], ignore_index=True, sort=False)).drop_duplicates(keep='first', inplace=False)
 # tr_concat.to_csv(path_or_buf='data/transactions_in_january_reduced_edited.csv', index=False)
 # lb_concat.to_csv(path_or_buf='data/february_labels_reduced_edited.csv', index=False)
+
+
+# ------------------------ split train test
+users = pd.read_csv('data/february_labels_reduced_edited.csv')
+trans = pd.read_csv('data/transactions_in_january_reduced_edited.csv')
+train_users, test_users = train_test_split(users, train_size=0.6)
+train_transactions = find_transactions(transactions=trans, users=train_users)
+test_transactions = find_transactions(transactions=trans, users=test_users)
+
+train_transactions.to_csv('data_split/train_transactions.csv', index=False)
+test_transactions.to_csv('data_split/test_transactions.csv', index=False)
+train_users.to_csv('data_split/train_labels.csv', index=False)
+test_users.to_csv('data_split/test_labels.csv', index=False)
+
+print("train_transactions = \n", train_transactions, '\n')
+print("test_transactions = \n", test_transactions)
