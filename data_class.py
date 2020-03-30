29b5ab98892273f956b0b641c813d5124eaf97d8
ElhamMirafzali <e.mirafzali@gmail.com>
2020-03-31 02:22:13 +0430

    converting data frame to tensor is updated.

--- data_class.py
+++ data_class.py
@@ -1,28 +1,25 @@
 import torch
 from torch.utils.data.dataset import Dataset
 import pandas as pd
+from preprocess import preprocess_transactions
 
 
 class SequentialDataset(Dataset):
     def __init__(self, transactions_path, targets_path):
-        self.transactions = pd.read_csv(transactions_path)
+        self.transactions = preprocess_transactions(transactions_path)
         self.targets = (pd.read_csv(targets_path)).sample(frac=1)
         self.users = self.targets['msno']
 
     def __getitem__(self, index: int):
-        dataset = self.transactions.loc[self.transactions['msno'] == self.users[index]]
+        dataset: pd.DataFrame = self.transactions.loc[self.transactions['msno'] == self.users[index]]
         dataset = dataset.sort_values('transaction_date')
         target = torch.tensor((self.targets.loc[self.targets['msno'] == self.users[index]]['is_churn']).squeeze(),
                               dtype=torch.double)
-        tensor_data = torch.tensor(data=dataset[[
-            'payment_method_id',
-            'payment_plan_days',
-            'plan_list_price',
-            'actual_amount_paid',
-            'is_auto_renew',
-            'transaction_date',
-            'membership_expire_date',
-            'is_cancel']].values, dtype=torch.double)
+
+        # 'msno', 'transaction_date', 'membership_expire_date' are removed
+        dataset = dataset[dataset.columns.difference(['msno', 'transaction_date', 'membership_expire_date'])]
+
+        tensor_data = torch.tensor(data=dataset.values, dtype=torch.double)
 
         return tensor_data, target
 
