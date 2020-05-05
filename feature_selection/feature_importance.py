bf4d73906ff8f23823cbed6a82e2070feb39f7d2
ElhamMirafzali <e.mirafzali@gmail.com>
2020-05-05 17:46:12 +0430

    Feature importance methods are updated for new features.

--- feature_selection/feature_importance.py
+++ feature_selection/feature_importance.py
@@ -3,8 +3,8 @@ from sklearn.ensemble import ExtraTreesClassifier
 import matplotlib.pyplot as plt
 from feature_selection.get_x_y import get_x_y
 
-X, y, _ = get_x_y(df1_path='../balanced_data_split/train_logs.csv',
-                  df2_path='../balanced_data_split/train_labels.csv',
+X, y, _ = get_x_y(df1_path='../new_data/selected2/train_logs_with_extracted_features.csv',
+                  df2_path='../new_data/selected2/train_labels.csv',
                   col='msno')
 
 model = ExtraTreesClassifier()
@@ -12,5 +12,5 @@ model.fit(X, y)
 print(model.feature_importances_)  # use inbuilt class feature_importances of tree based classifiers
 # plot graph of feature importances for better visualization
 feature_importances = pd.Series(model.feature_importances_, index=X.columns)
-feature_importances.nlargest(10).plot(kind='barh', color=['b', 'g', 'r', 'c', 'm', 'y', 'g'])
+feature_importances.nlargest(15).plot(kind='barh', color=['b', 'g', 'r', 'c', 'm', 'y', 'g'])
 plt.show()
--- feature_selection/univariate_selection.py
+++ feature_selection/univariate_selection.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.feature_selection import SelectKBest, chi2
 from feature_selection.get_x_y import get_x_y
 
-X, y, _ = get_x_y(df1_path='../balanced_data_split/train_logs.csv',
-                  df2_path='../balanced_data_split/train_labels.csv',
+X, y, _ = get_x_y(df1_path='../new_data/selected2/train_logs_with_extracted_features.csv',
+                  df2_path='../new_data/selected2/train_labels.csv',
                   col='msno')
 
 # apply SelectKBest class to score the features
@@ -14,4 +14,4 @@ columns = pd.DataFrame(X.columns)
 # concat two data frames for better visualization
 featureScores = pd.concat([columns, scores], axis=1)
 featureScores.columns = ['Feature', 'Score']  # naming the data frame columns
-print(featureScores.nlargest(10, 'Score'))  # print 10 best features
+print(featureScores.nlargest(15, 'Score'))  # print 10 best features
