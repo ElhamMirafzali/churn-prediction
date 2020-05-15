b11167ed0079adca41c5afaf4f3ccec49b78077b
ElhamMirafzali <e.mirafzali@gmail.com>
2020-05-16 00:00:25 +0430

    Transactions feature extraction and preprocessing are separated from data_class.

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+from datetime import date
 
 
 def get_data_frame(path):
@@ -10,12 +11,11 @@ def get_data_frame(path):
     return df
 
 
-def extract_features(input_df, start_date, destination_path):
+def extract_features_of_logs(input_df, start_date, destination_path):
     users = input_df.msno.unique()
-    users_num = len(users)
     user_index = 0
     for user in users:
-        sequence = data_frame.loc[data_frame['msno'] == user].sort_values('date').reset_index(drop=True)
+        sequence = input_df.loc[input_df['msno'] == user].sort_values('date').reset_index(drop=True)
         for i in range(len(sequence)):
             if i == 0:
                 sequence.loc[i, 'last_log_date_of_25'] = start_date
@@ -75,9 +75,47 @@ def extract_features(input_df, start_date, destination_path):
             print('Number of completed users = %d' % user_index)
 
 
-logs = 'new_data/selected2/test_logs.csv'
-data_frame: pd.DataFrame = get_data_frame(logs)
-print("Data frame is ready.")
+def avg_time_between_trans(dataset):
+    for i, row in dataset.iterrows():
+        if i == 0:
+            dataset.loc[i, 'avg_time_between_trans'] = 0
+        else:
+            date1 = str(dataset.loc[i - 1, 'transaction_date'])
+            date2 = str(dataset.loc[i, 'transaction_date'])
+            date1 = date(year=int(date1[0:4]), month=int(date1[4:6]), day=int(date1[6:8]))
+            date2 = date(year=int(date2[0:4]), month=int(date2[4:6]), day=int(date2[6:8]))
+            dataset.loc[i, 'avg_time_between_trans'] = (((date2 - date1).days + (
+                    dataset.loc[i - 1, 'avg_time_between_trans'] * (i - 1))) / i)
 
-# extract_features(input_df=data_frame, start_date=20170301,
+    return dataset
+
+
+def extract_features_of_transactions(input_df, destination_path):
+    users = input_df.msno.unique()
+    user_index = 0
+    for user in users:
+        sequence = input_df.loc[input_df['msno'] == user].sort_values('transaction_date').reset_index(drop=True)
+        trans_data = avg_time_between_trans(sequence)
+        if not os.path.isfile(destination_path):
+            trans_data.to_csv(destination_path, index=False)
+        else:
+            trans_data.to_csv(destination_path, mode='a', index=False, header=False)
+
+        user_index += 1
+        if user_index % 500 == 0:
+            print('Number of completed users = %d' % user_index)
+
+# logs = 'new_data/selected2/test_logs.csv'
+# data_frame: pd.DataFrame = get_data_frame(logs)
+# print("Data frame is ready.")
+
+# extract_features_of_logs(input_df=data_frame, start_date=20170301,
 #                  destination_path='new_data/selected2/test_logs_with_extracted_features.csv')
+
+
+# transactions_path = 'new_data/selected2/test_transactions.csv'
+# data_frame: pd.DataFrame = get_data_frame(transactions_path)
+# print("Data frame is ready.")
+#
+# extract_features_of_transactions(input_df=data_frame,
+#                                  destination_path='new_data/selected2/test_transactions_with_extracted_features.csv')
--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn import preprocessing
-from datetime import date
 
 
-def preprocess_transactions(data_path):
-    data = pd.read_csv(data_path)
+def preprocess_transactions(train_path, test_path):
+    train_trans = pd.read_csv(train_path)
+    test_trans = pd.read_csv(test_path)
+    data = pd.concat([train_trans, test_trans], ignore_index=True)
 
     # filling missing values with 0
     data = data.fillna(0)
@@ -12,13 +13,22 @@ def preprocess_transactions(data_path):
     # normalization
     data[['payment_plan_days',
           'plan_list_price',
-          'actual_amount_paid']] = preprocessing.normalize(data[['payment_plan_days',
-                                                                 'plan_list_price',
-                                                                 'actual_amount_paid']])
+          'actual_amount_paid',
+          'avg_time_between_trans']] = preprocessing.normalize(data[['payment_plan_days',
+                                                                     'plan_list_price',
+                                                                     'actual_amount_paid',
+                                                                     'avg_time_between_trans']])
+
     # one hot encoding
     data = pd.get_dummies(data, columns=["payment_method_id"], prefix=["payment_method"])
 
-    return data
+    train_trans_normalized = data[0:len(train_trans)]
+    test_trans_normalized = data[len(train_trans):]
+
+    train_trans_normalized.to_csv('new_data/selected2/train_transactions_with_extracted_features_preprocessed.csv', index=False)
+    test_trans_normalized.to_csv('new_data/selected2/test_transactions_with_extracted_features_preprocessed.csv', index=False)
+
+    return train_trans_normalized, test_trans_normalized
 
 
 def preprocess_logs(train_path, test_path):
@@ -86,22 +96,10 @@ def preprocess_members(data_path):
     return data
 
 
-def avg_time_between_trans(dataset):
-    for i, row in dataset.iterrows():
-        if i == 0:
-            dataset.loc[i, 'avg_time_between_trans'] = 0
-        else:
-            date1 = str(dataset.loc[i - 1, 'transaction_date'])
-            date2 = str(dataset.loc[i, 'transaction_date'])
-            date1 = date(year=int(date1[0:4]), month=int(date1[4:6]), day=int(date1[6:8]))
-            date2 = date(year=int(date2[0:4]), month=int(date2[4:6]), day=int(date2[6:8]))
-            dataset.loc[i, 'avg_time_between_trans'] = (((date2 - date1).days + (
-                    dataset.loc[i - 1, 'avg_time_between_trans'] * (i - 1))) / i)
-    # normalize new column
-    dataset[['avg_time_between_trans']] = preprocessing.normalize(dataset[['avg_time_between_trans']])
-    return dataset
-
 # preprocess_logs(train_path='new_data/selected2/train_logs_with_extracted_features.csv',
 #                 test_path='new_data/selected2/test_logs_with_extracted_features.csv')
 
 # preprocess_members(data_path='data/members.csv')
+
+# preprocess_transactions(train_path='new_data/selected2/train_transactions_with_extracted_features.csv',
+#                         test_path='new_data/selected2/test_transactions_with_extracted_features.csv')
