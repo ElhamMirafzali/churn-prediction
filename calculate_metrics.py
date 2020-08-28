7203758237d6cfdd03349fa44ce210037aa3e3d3
ElhamMirafzali <e.mirafzali@gmail.com>
2020-08-28 23:27:24 +0430

    precision metric is added.

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -6,16 +6,17 @@ def calculate_metrics(targets, predictions, bin_predictions):
     auc = metrics.roc_auc_score(targets, predictions)
 
     # calculate precision-recall curve
-    precision, recall, _ = metrics.precision_recall_curve(targets, predictions)
-    pr_auc = metrics.auc(recall, precision)
+    precision_arr, recall_arr, _ = metrics.precision_recall_curve(targets, predictions)
+    pr_auc = metrics.auc(recall_arr, precision_arr)
 
     # calculate precision
     average_precision = metrics.average_precision_score(targets, predictions)
+    precision = metrics.precision_score(targets, bin_predictions)
 
     # calculate recall
-    average_recall = metrics.recall_score(targets, bin_predictions)
+    recall = metrics.recall_score(targets, bin_predictions)
 
     # calculate F1 score
     f1 = metrics.f1_score(targets, bin_predictions)
 
-    return auc, pr_auc, average_precision, average_recall, f1
+    return auc, pr_auc, average_precision, precision, recall, f1
